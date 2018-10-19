c3cda9044b69d4d23ed925c7f6e9265141297165
ryan mccleave <ryan.mccleave@unosquare.com>
2018-10-19 11:41:14 +0100

    Task TS-120 - Seed database with an admin user

---
+++ src/main/resources/db/migration/V1.7__add_admin_user.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.employee (
+                            employee_id,
+                            country_id,
+                            email,
+                            employee_role_id,
+                            employee_status_id,
+                            forename,
+                            password,
+                            start_date,
+                            surname,
+                            total_holidays
+                            )
+VALUES (1, 1, 'admin@unosquare.com', 2, 1, 'Admin', '$2a$10$1Mwd1EZPECnx6UspIEWd0.noAuyKmsxrLUAztQb1uMpg/RkagLIgO', '2018-01-01', 'User', 33);
