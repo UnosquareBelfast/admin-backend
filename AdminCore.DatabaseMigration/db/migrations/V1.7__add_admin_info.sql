f7f28b84ded5c79c29204faa09fac65834de9dad
nmcmahon-unosquare <niall.mcmahon@unosquare.com>
2019-01-29 14:41:47 +0000

    Added DB seeding scripts to migration

--- AdminCore.DatabaseMigration/db/migrations/V1.7__add_admin_info.sql
+++ AdminCore.DatabaseMigration/db/migrations/V1.7__add_admin_info.sql
@@ -1,8 +1,8 @@
 ﻿INSERT INTO Employee(country_id, email, employee_role_id, employee_status_id, forename, password, start_date, surname, total_holidays, deleted)
-VALUES (1, 'eoin.mcafee@unosquare.com', 2, 1, 'Eoin','', '2019-01-27','McAfee', 33, TRUE);
+VALUES (1, 'eoin.mcafee@unosquare.com', 2, 1, 'Eoin','', '2019-01-27','McAfee', 33, false);
 INSERT INTO Employee(country_id, email, employee_role_id, employee_status_id, forename, password, start_date, surname, total_holidays, deleted)
-VALUES (1, 'roisin.hughes@unosquare.com', 2, 1, 'Roisin','', '2019-01-27','Hughes', 33, TRUE);
+VALUES (1, 'roisin.hughes@unosquare.com', 2, 1, 'Roisin','', '2019-01-27','Hughes', 33, false);
 INSERT INTO Employee(country_id, email, employee_role_id, employee_status_id, forename, password, start_date, surname, total_holidays, deleted)
-VALUES (1, 'katrina.jones@unosquare.com', 2, 1, 'Katrina','', '2019-01-27','Jones', 33, TRUE);
+VALUES (1, 'katrina.jones@unosquare.com', 2, 1, 'Katrina','', '2019-01-27','Jones', 33, false);
 INSERT INTO Employee(country_id, email, employee_role_id, employee_status_id, forename, password, start_date, surname, total_holidays, deleted)
-VALUES (1, 'mark.brown@unosquare.com', 2, 1, 'Mark','', '2019-01-27','Brown', 33, TRUE);
+VALUES (1, 'mark.brown@unosquare.com', 2, 1, 'Mark','', '2019-01-27','Brown', 33, false);
