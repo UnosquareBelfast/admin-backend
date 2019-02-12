b74cdc4717b7dedee7d0c49df723596bf1da9ddf
Jamie Higgins <jamie.higgins@unosquare.com>
2019-02-12 10:47:39 +0000

    Added further event types
    Additional Event Types added
    Sickness is Auto Approved

--- AdminCore.Constants/Enums/EventTypes.cs
+++ AdminCore.Constants/Enums/EventTypes.cs
@@ -8,8 +8,36 @@
 
     WorkingFromHome = 2,
 
-    SickLeave = 3,
+    Bereavement = 3,
 
-    WorkRelatedTravel = 4
+    JuryService = 4,
+
+    OnBusiness = 5,
+
+    ParentalLeave = 6,
+
+    SharedParentalLeave = 7,
+
+    MaternityLeave = 8,
+
+    PaternityLeave = 9,
+
+    Adoption = 10,
+
+    Medical = 11,
+
+    Sickness = 12,
+
+    Training = 13,
+
+    Unauthorised = 14,
+
+    Wedding = 15,
+
+    Compassionate = 16,
+
+    EmergencyLeaveForDependance = 17,
+
+    MovingHouse = 18
   }
 }
--- AdminCore.DatabaseMigration/db/migrations/V1.5__add_event_types.sql
+++ AdminCore.DatabaseMigration/db/migrations/V1.5__add_event_types.sql
@@ -2,7 +2,21 @@ INSERT INTO public.event_type (event_type_id, description)
 VALUES (0, 'Public Holiday'),
 			 (1, 'Annual Leave'),
        (2, 'Working From Home'),
-       (3, 'Sick Leave'),
-       (4, 'Work Related Travel')
+       (3, 'Bereavement'),
+       (4, 'Jury Service'),
+			 (5, 'On Business'),
+			 (6, 'Parental Leave'),
+			 (7, 'Shared Parental Leave'),
+       (8, 'Maternity Leave'),
+			 (9, 'Paternity Leave'),
+			 (10, 'Adoption Leave'),
+			 (11, 'Medical Leave'),
+       (12, 'Sick Leave'),
+			 (13, 'Training'),
+			 (14, 'Unauthorised Leave'),
+			 (15, 'Wedding'),
+       (16, 'Compassionate'),
+			 (17, 'Emergency Leave For Dependance'),
+			 (18, 'Moving House')
 ON CONFLICT (event_type_id)
   DO NOTHING;
