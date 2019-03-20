e040fb7183b955ead03afd9388fc067d065a2f83
Jamie Higgins <jamie.higgins@unosquare.com>
2019-03-20 14:21:03 +0000

    Failing Test

--- AdminCore.Services.Tests/UnitTest1.cs
+++ AdminCore.Services.Tests/UnitTest1.cs
@@ -1,23 +1,49 @@
-﻿using AdminCore.Common.Interfaces;
-using AdminCore.DAL;
-using AdminCore.DAL.Entity_Framework;
+﻿using AdminCore.DAL.Entity_Framework;
 using AdminCore.DAL.Models;
-using AdminCore.Services.Mappings;
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using AdminCore.DAL.Database;
+using AdminCore.DAL;
 using Xunit;
 
 namespace AdminCore.Services.Tests
 {
-  public class UnitTest1
+  public class UnitTest1 : BaseContextSetUp
   {
-    protected static DbSet<T> GetQueryableMockDbSet<T>(IList<T> sourceList) where T : class
+    [Fact]
+    public void Test1()
+    {
+      const int eventId = 1;
+      const int eventDateId = 1;
+
+      var eventDate = new EventDate
+      {
+        EventDateId = eventDateId,
+        EventId = eventId,
+        StartDate = new DateTime(2018, 12, 03),
+        EndDate = new DateTime(2018, 12, 05)
+      };
+
+      var databaseContext = Substitute.ForPartsOf<EntityFrameworkContext>();
+      var eventDates = GetQueryableMockDbSet(new List<EventDate> { eventDate });
+
+      databaseContext.When(x => x.Set<EventDate>()).DoNotCallBase();
+      databaseContext.Set<EventDate>().Returns(eventDates);
+
+      var eventDateRepository = new EntityFrameworkRepository<EventDate>(databaseContext);
+      IRepository<EventDate> eventDateRepo = eventDateRepository;
+      databaseContext.EventDatesRepository.Returns(eventDateRepo);
+
+      var resultOne = databaseContext.EventDatesRepository.GetSingle(x => x.EventDateId == eventId);
+      var resultTne = databaseContext.EventDatesRepository.GetSingle(x => x.EventDateId == 0);
+
+      Assert.NotNull(resultOne);
+      Assert.Null(resultTne);
+    }
+
+    protected new static DbSet<T> GetQueryableMockDbSet<T>(IList<T> sourceList) where T : class
     {
       var queryable = sourceList.AsQueryable();
       var dbSet = Substitute.For<DbSet<T>, IQueryable<T>>();
@@ -29,51 +55,6 @@ namespace AdminCore.Services.Tests
       ((IQueryable<T>)dbSet).GetEnumerator().Returns(enumerator).AndDoes(x => enumerator.Reset());
 
       return dbSet;
-    }
-
-    //protected DbSet<T> SetupRepositoryForContext<T>(IList<T> sourceList) where T : class
-    //{
-    //  var dbSet = GetQueryableMockDbSet(sourceList);
-    //  ((EntityFrameworkContext)DatabaseContext).Set<T>().Returns(x => dbSet);
-    //  DatabaseContext.Attach(Arg.Any<T>()).Returns(x => (T)x[0]);
-
-    //  dbSet.Include(Arg.Any<string>()).Returns(dbSet);
-
-    //  return dbSet;
-    //}
-    [Fact]
-    public void Test1()
-    {
-      const int eventId = 1;
-
-      var eventDate = new EventDate
-      {
-        EventId = eventId,
-        StartDate = new DateTime(2018, 12, 03),
-        EndDate = new DateTime(2018, 12, 05)
-      };
-
-      var mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(new EventMapperProfile())));
-
-      var databaseContext = Substitute.For<IDatabaseContext, EntityFrameworkContext>();
-      var dateService = Substitute.For<IDateService>();
-
-      ((EntityFrameworkContext)databaseContext).Set<EventDate>()
-        .Returns(GetQueryableMockDbSet(new List<EventDate> { eventDate }));
-
-      var eventRepository = new EntityFrameworkRepository<Event>(databaseContext);
-
-      var eventService = new EventService(databaseContext, mapper, dateService);
-
-      databaseContext.EventRepository.Returns(eventRepository);
-
-      // eventRepository.GetSingle()
-
-      // Assert
-      var eventDateDto = eventService.GetEvent(1);
-
-      Mock<IRepository<EventDate>> mockEventDateRepository = new Mock<IRepository<EventDate>>();
-      mockEventDateRepository.Setup(m => m.GetSingle(x => x.EventDateId == 1)).Returns(eventDate);
     }
   }
 }
