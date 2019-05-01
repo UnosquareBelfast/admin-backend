e864afbbe14e1f5775a79184007c193bb8d5874b
Kacper Kuczkowski <kacper.kuczkowski@unosquare.com>
2019-05-01 16:46:30 +0100

    Add SMTP sender

---
+++ AdminCore.MailClients/SMTP/SmtpSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdminCore.MailClients.SMTP
+{
+    /// <summary>
+    /// Dispatches email messages to mail server using MailKit SMTP Client.
+    /// </summary>
+    public class SmtpSender
+    {
+        
+    }
+}
