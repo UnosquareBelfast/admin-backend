d71812b9e316eaff1e867aa8eae5b7c4cfbf0aba
Richard Gillen <richard.gillen@unosquare.com>
2019-05-13 14:54:19 +0100

    Add comments to ICsvService.

--- AdminCore.Common/Interfaces/ICsvService.cs
+++ AdminCore.Common/Interfaces/ICsvService.cs
@@ -5,6 +5,13 @@ namespace AdminCore.Common.Interfaces
 {
   public interface ICsvService
   {
+    /// <summary>
+    /// Generate a csv from the given passed in data.
+    /// Class type must be decorated with the required common attributes.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Byte[] Generate<T>(IList<T> data) where T : class;
   }
 }
