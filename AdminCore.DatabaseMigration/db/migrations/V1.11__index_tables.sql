1a785e1ecf43d12e432494fbf3b5bde4bb32ed59
Richard Gillen <richard.gillen@unosquare.com>
2019-05-17 17:31:46 +0100

    LM-26: Add new tables to migrations for indexing

---
+++ AdminCore.DatabaseMigration/db/migrations/V1.11__index_tables.sql
@@ -0,0 +1,78 @@
+/*
+Description:
+Adds indexing to specific tables that are used often/contain many data to help improve performance
+*/
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EVENT TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX event_event_type_id_idx ON public.event(event_type_id);
+CREATE INDEX event_employee_id_idx ON public.event(employee_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    CONTRACT TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX contract_employee_id_idx ON public.contract(employee_id);
+CREATE INDEX contract_team_id_idx ON public.contract(team_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    TEAM TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX team_team_id_idx ON public.team(team_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EVENT DATES TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX event_dates_event_date_id_idx ON public.event_date(event_date_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EVENT TYPE TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX event_type_event_type_id_idx ON public.event_type(event_type_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EMPLOYEE TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX employee_employee_id_idx ON public.employee(employee_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EVENT STATUS TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX event_status_event_status_id_idx ON public.event_status(event_status_id);
+
+----------------------------------------------------------------------------------------
+
+/*
+                                    EVENT MESSAGE TABLE
+*/
+
+----------------------------------------------------------------------------------------
+CREATE INDEX event_message_event_message_id_idx ON public.event_message(event_message_id);
