7bf9ff9baba3638faa7ab42f1f7ac8703406844f
Richard Gillen <richard.gillen@unosquare.com>
2019-05-21 15:31:14 +0100

    LM-13-1: Add to migration to ensure default project created for every team/client.

--- AdminCore.DatabaseMigration/db/migrations/V1.11__add_project.sql
+++ AdminCore.DatabaseMigration/db/migrations/V1.11__add_project.sql
@@ -37,6 +37,8 @@ ALTER SEQUENCE employee_employee_id_seq
 */
 
 ----------------------------------------------------------------------------------------
+INSERT INTO public.project (project_name, client_id, deleted)
+SELECT team_name, client_id, false FROM public.team;
 
 -- Alter existing tables
 ----------------------------------------------------------------------------------------
@@ -52,6 +54,25 @@ ALTER TABLE public.team
   ADD CONSTRAINT team_project_id_fkey FOREIGN KEY (project_id)
     REFERENCES public.project (project_id) MATCH SIMPLE
     ON UPDATE NO ACTION
-    ON DELETE NO ACTION,
+    ON DELETE NO ACTION;
 
-  DROP COLUMN client_id;
+-- Set the project_id column on team to the project id that matches the old team => client relationship.
+-- Ensure that there is a default project created for every existing team/client and the foreign keys are assigned correctly before dropping data.
+UPDATE public.team
+SET
+  project_id = project.project_id
+FROM
+  project
+WHERE
+  team.client_id = project.client_id;
+
+-- Drop columns
+----------------------------------------------------------------------------------------
+
+/*
+                                   TEAM TABLE
+*/
+
+----------------------------------------------------------------------------------------
+ALTER TABLE public.team
+DROP COLUMN client_id;
