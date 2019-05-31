1cd97bc6dedd310a543283a86e18ef4644cb3678
Kacper Kuczkowski <kacper.kuczkowski@unosquare.com>
2019-05-31 11:56:49 +0100

    Add decode funtion to EventRequest link generator

--- AdminCore.LinkGenerator/Interfaces/ILinkGenerator.cs
+++ AdminCore.LinkGenerator/Interfaces/ILinkGenerator.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Text;
 using AdminCore.DTOs.LinkGenerator;
 
-namespace RequestLinkGenerator.Interfaces
+namespace AdminCore.LinkGenerator.Interfaces
 {
     public interface ILinkGenerator
     {
         EventRequestDto CreateRequest(int eventId, int eventDateId, int requestTypeId, int requestLifeCycle, DateTime timeCreated);
         HashedLinkDto GenerateLink(EventRequestDto eventRequestDto);
+        int Decode(string salt, string hash);
     }
 }
--- AdminCore.LinkGenerator/LinkGenerators/EventRequest.cs
+++ AdminCore.LinkGenerator/LinkGenerators/EventRequest.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Linq;
 using AdminCore.DTOs.LinkGenerator;
+using AdminCore.LinkGenerator.Interfaces;
 using HashidsNet;
-using RequestLinkGenerator.Interfaces;
 
-namespace RequestLinkGenerator.LinkGenerators
+namespace AdminCore.LinkGenerator.LinkGenerators
 {
     /// <summary>
     /// Creates Event Request obj and generates link for that obj
@@ -17,6 +18,7 @@ namespace RequestLinkGenerator.LinkGenerators
         /// <param name="eventDateId">Event Date Id</param>
         /// <param name="requestTypeId">Request Type Id</param>
         /// <param name="requestLifeCycle">Request Life Cycle, expected multiple of 24</param>
+        /// <param name="timeCreated">Time request was created</param>
         /// <returns>EventRequestDto obj</returns>
         /// <exception cref="Exception">Is thrown given invalid requestLifeCycle</exception>
         public EventRequestDto CreateRequest(int eventId, int eventDateId, int requestTypeId, int requestLifeCycle, DateTime timeCreated)
@@ -30,10 +32,9 @@ namespace RequestLinkGenerator.LinkGenerators
             return new EventRequestDto
             {
                 RequestTypeId = requestTypeId,
-                EventId = eventId,
                 EventDateId = eventDateId,
                 Salt = salt,
-                Hash = new Hashids(salt).Encode(eventId),
+                Hash = new Hashids(salt, HashLength).Encode(eventId),
                 TimeCreated =  timeCreated,
                 TimeExpires = CalculateRequestExpirationDateTime(timeCreated, requestLifeCycle),
                 Expired = false,
@@ -42,7 +43,7 @@ namespace RequestLinkGenerator.LinkGenerators
         }
 
         /// <summary>
-        /// 
+        /// todo
         /// </summary>
         /// <param name="eventRequest"></param>
         /// <returns></returns>
@@ -56,6 +57,17 @@ namespace RequestLinkGenerator.LinkGenerators
 
             // todo consider domain, route to be configurable
             return new HashedLinkDto("localhost:8081", "event-response", eventRequest.Hash);
+        }
+
+        public int Decode(string salt, string hash)
+        {
+            var chunkedEventId = new Hashids(salt, HashLength).Decode(hash);
+
+            if (chunkedEventId.Length == 0)
+            {
+                throw new Exception($"event Id unknown, did not recognise {hash} request");
+            }
+            return chunkedEventId.Select((t, i) => t * Convert.ToInt32(Math.Pow(10, chunkedEventId.Length - i - 1))).Sum();
         }
 
         private static DateTime CalculateRequestExpirationDateTime(DateTime timeCreated, int requestLifeCycle)
@@ -77,6 +89,8 @@ namespace RequestLinkGenerator.LinkGenerators
             }
             return timeCreated;
         }
+
+        private const int HashLength = 16;
         private const int Day = 24;
     }
 }
