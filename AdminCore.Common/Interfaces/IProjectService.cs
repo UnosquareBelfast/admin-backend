f2cd5a1b981f3ba350093b4124d914483cfb3b25
Richard Gillen <richard.gillen@unosquare.com>
2019-06-07 12:18:50 +0100

    LM-56: Update project interface.

--- AdminCore.Common/Interfaces/IProjectService.cs
+++ AdminCore.Common/Interfaces/IProjectService.cs
@@ -3,14 +3,14 @@ using AdminCore.DTOs.Project;
 
 namespace AdminCore.Common.Interfaces
 {
-  public interface IProjectService
-  {
-    ProjectDto CreateProject(ProjectDto projectToCreate);
-    ProjectDto UpdateProject(ProjectDto projectToUpdate);
-    void DeleteProject(int projectId);
+    public interface IProjectService
+    {
+        ProjectDto CreateProject(ProjectDto projectToCreate);
+        ProjectDto UpdateProject(ProjectDto projectToUpdate);
+        void DeleteProject(int projectId);
 
-    IList<ProjectDto> GetProjects();
-    IList<ProjectDto> GetProjectsById(int projectId);
-    IList<ProjectDto> GetProjectsByClientId(int clientId);
-  }
+        IList<ProjectDto> GetProjects();
+        IList<ProjectDto> GetProjectsById(int projectId);
+        IList<ProjectDto> GetProjectsByClientId(int clientId);
+    }
 }
